356cde47b357db778bfa0cef21abae61d321065e
Ziad Hamed <55656747+Ziad-Ziad@users.noreply.github.com>
2023-03-14 03:32:39 +0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions',
+      version='1.0',
+      description='Gaussian and binominal distributions',
+      packages=['distributions'],
+      zip_safe=False)
